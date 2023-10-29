d2fa72bb4f2d912033e55ec51a44a60475d23596
SunilKorapana <125741839+SunilKorapana@users.noreply.github.com>
2023-10-29 21:11:23 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -12,11 +12,11 @@ const todoListinDatabase = ref(database, "todoLists");
 const inputEl = document.getElementById("input-el");
 const buttonEl = document.getElementById("button-el");
 const listsEl = document.getElementById("lists-el");
-const deleteEl = document.getElementById("delete-el");
+const deleteEl = document.getElementById("delete-el")
 
 buttonEl.addEventListener("click", function(){
     let inputValue = inputEl.value;
-    push(todoListinDatabase, { task: inputValue, completed: false }); // Store tasks with completion status
+    push(todoListinDatabase, inputValue);
     clearInputField();
 });
 
@@ -24,48 +24,52 @@ function clearInputField(){
     inputEl.value = "";
 }
 
+
 function render(tasks) {
     if (tasks.length === 0) {
         listsEl.innerHTML = "<p>No tasks yet! Add a task above.</p>";
     } else {
-        let allTasks = "";
-        tasks.forEach((task, index) => {
-            const isCompleted = task.completed || false; // Check completion status
-            const completedClass = isCompleted ? 'completed' : ''; // Add a class for completed tasks
-
-            allTasks += `
-                <div class="task-item ${completedClass}" id="task-item-${index}">
-                    <input type="checkbox" id="checkbox-${index}" style="width: 20px; height: 20px;" ${isCompleted ? 'checked' : ''} />
-                    <label for="checkbox-${index}">
-                        <span id="task-${index}">${task.task}</span>
-                    </label>
-                </div>
+    let allTasks = "";
+    tasks.forEach((task, index) => {
+        allTasks += `
+            <div class="task-item" id="task-item-${index}">
+                <input type="checkbox" id="checkbox-${index}" style="width: 20px; height: 20px;" />
+                <label for="checkbox-${index}">
+                    <span id="task-${index}">${task}</span>
+                </label>
+            </div>
             `;
         });
         listsEl.innerHTML = allTasks;
-
-        tasks.forEach((task, index) => {
-            const checkbox = document.getElementById(`checkbox-${index}`);
-            const taskItem = document.getElementById(`task-item-${index}`);
-            const taskItemRef = ref(database, `todoLists/${index}/completed`);
-
-            checkbox.addEventListener('change', function() {
-                const isChecked = checkbox.checked;
-                set(taskItemRef, isChecked); // Update completion status in the database
-            });
-
-            onValue(taskItemRef, snapshot => {
-                const isCompleted = snapshot.val();
-                if (isCompleted) {
-                    taskItem.classList.add('completed');
-                    checkbox.checked = true;
-                } else {
-                    taskItem.classList.remove('completed');
-                    checkbox.checked = false;
-                }
-            });
-        });
     }
+    
+    tasks.forEach((_, index) => {
+        const checkbox = document.getElementById(`checkbox-${index}`);
+        const taskItem = document.getElementById(`task-item-${index}`);
+        const taskItemRef = ref(database, `completed/${index}`);
+
+        checkbox.addEventListener('change', function() {
+            const isChecked = checkbox.checked;
+            if (isChecked) {
+                taskItem.style.backgroundColor = 'lightgreen'; // Change background color of the entire task item
+                set(taskItemRef, true); // Update completion status in the database
+            } else {
+                taskItem.style.backgroundColor = ''; // Reset background color
+                set(taskItemRef, false); // Update completion status in the database
+            }
+        });
+
+        onValue(taskItemRef, snapshot => {
+            const isCompleted = snapshot.val();
+            if (isCompleted) {
+                taskItem.style.backgroundColor = 'lightgreen';
+                checkbox.checked = true;
+            } else {
+                taskItem.style.backgroundColor = ''; // Set background to default if not completed
+                checkbox.checked = false;
+            }
+        });
+    });
 }
 
 deleteEl.addEventListener("click", () => {
@@ -78,8 +82,8 @@ deleteEl.addEventListener("click", () => {
             console.error("Error deleting tasks:", error);
             // You might want to handle errors, show a message, etc.
         });
-    } else {
-        console.log("Cancelled deletion of tasks.");
+    }else{
+        message.content = "no tasks"
     }
 });
 
@@ -87,4 +91,4 @@ onValue(todoListinDatabase, (snapshot) => {
     const data = snapshot.val();
     const tasks = Object.values(data || []);
     render(tasks);
-});
+})
